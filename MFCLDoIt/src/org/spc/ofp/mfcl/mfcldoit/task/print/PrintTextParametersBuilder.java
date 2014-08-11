1cba0081e15a4d3ba562c472d0793b68fb29ff5b
Fabrice Bouyé <fabriceb@spc.int>
2014-08-11 15:07:59 +1100

    Added preliminary support for simple text printing.

---
+++ MFCLDoIt/src/org/spc/ofp/mfcl/mfcldoit/task/print/PrintTextParametersBuilder.java
@@ -0,0 +1,91 @@
+/***********************************************************************
+ *  Copyright - Secretariat of the Pacific Community                   *
+ *  Droit de copie - Secrétariat Général de la Communauté du Pacifique *
+ *  http://www.spc.int/                                                *
+ ***********************************************************************/
+package org.spc.ofp.mfcl.mfcldoit.task.print;
+
+import javafx.scene.text.Font;
+import javafx.scene.text.TextAlignment;
+import javafx.stage.Window;
+
+/**
+ * Builder class for the parameters object.
+ * @author Fabrice Bouyé (fabriceb@spc.int)
+ */
+public final class PrintTextParametersBuilder {
+
+    /**
+     * Hidden constructor.
+     */
+    private PrintTextParametersBuilder() {
+    }
+
+    /**
+     * Creates a new instance.
+     * @return A {@code PrintTextParametersBuilder} instance, never {@code null}.
+     */
+    public static PrintTextParametersBuilder create() {
+        return new PrintTextParametersBuilder();
+    }
+
+    /**
+     * Build the parameters object.
+     * @return A {@code PrintTextParameters} instance, never {@code null}.
+     */
+    public PrintTextParameters build() {
+        final PrintTextParameters result = new PrintTextParameters();
+        result.text = text;
+        result.font = font;
+        result.textAlignment = textAlignment;
+        result.owner = owner;
+        return result;
+    }
+
+    private String text;
+
+    /**
+     * Sets the text.
+     * @param value The new value.
+     * @return A {@code PrintTextParametersBuilder} instance, never {@code null}.
+     */
+    public PrintTextParametersBuilder text(final String value) {
+        text = value;
+        return this;
+    }
+
+    private Font font;
+
+    /**
+     * Sets the font.
+     * @param value The new value.
+     * @return A {@code PrintTextParametersBuilder} instance, never {@code null}.
+     */
+    public PrintTextParametersBuilder font(final Font value) {
+        font = value;
+        return this;
+    }
+    private TextAlignment textAlignment;
+
+    /**
+     * Sets the text alignment.
+     * @param value The new value.
+     * @return A {@code PrintTextParametersBuilder} instance, never {@code null}.
+     */
+    public PrintTextParametersBuilder textAlignment(final TextAlignment value) {
+        textAlignment = value;
+        return this;
+    }
+
+    private Window owner;
+
+    /**
+     * Sets the owner window.
+     * @param value The new value.
+     * @return A {@code PrintTextParametersBuilder} instance, never {@code null}.
+     */
+    public PrintTextParametersBuilder owner(final Window value) {
+        owner = value;
+        return this;
+    }
+}
